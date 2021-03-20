8dfd84d746879445352b99137814ccceb77622d0
shyrkatawi <shyrkatawi@gmail.com>
2021-03-21 01:40:16 +0300

    refactor io task

--- src/main/java/io_fundamentals/Runner.java
+++ src/main/java/io_fundamentals/Runner.java
@@ -1,4 +1,4 @@
-package ioFundamentals;
+package io_fundamentals;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
  * путь к директории, например "D:/movies".
  * Записать в текстовый файл структуру папок и файлов в виде,
  * похожем на выполнение программы tree /F.
- *
+ * <p>
  * Если в качестве параметра в программу передается не путь к директории,
  * а путь к txt файлу по образцу выше - прочитать файл и вывести в консоль следующие данные:
  * Количество папок
@@ -19,13 +19,15 @@ import java.nio.file.Paths;
  */
 public class Runner {
     public static void main(String[] args) {
+//        String path = "D:\\tmp\\";
+//        String path = "D:\\output.txt";
         String path = args[0].replace("/", "\\");
-//        String path = "E:\\tmp\\";
-//        String path = "E:\\output.txt";
         if (path.endsWith(".txt")) {
             readFile(path);
         } else {
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter("E:\\output.txt"))) {
+            String folder = "D:\\";
+            String outputFile = "output.txt";
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(folder + outputFile))) {
                 writeStructureOfDirectory(writer, new File(path), "");
             } catch (IOException e) {
                 e.printStackTrace();
@@ -34,7 +36,7 @@ public class Runner {
     }
 
     private static void writeStructureOfDirectory(BufferedWriter writer, File f, String space) throws IOException {
-        final String spaceExample = "——";
+        final String spaceExample = "———";
         for (File tmpFile : f.listFiles()) {
             StringBuilder sb = new StringBuilder();
             sb.append(space);
@@ -60,19 +62,18 @@ public class Runner {
                     directoryCounter++;
                 } else {
                     fileCounter++;
-                    sumOfFileNamesLength += line.replaceAll("—", "").length();
-                    //до начала слова
+                    sumOfFileNamesLength += line.replace("———", "").length();
                 }
             }
             System.out.println(directoryCounter);
             System.out.println(fileCounter);
-            System.out.println((double)directoryCounter/fileCounter);
-            System.out.println((double)sumOfFileNamesLength/fileCounter);
+            if (fileCounter != 0) {
+                System.out.println((double) directoryCounter / fileCounter);
+                System.out.println((double) sumOfFileNamesLength / fileCounter);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
 }
-
-
